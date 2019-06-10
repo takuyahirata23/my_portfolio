143434b5e4bcd22054687c75e0678bb679bcb2e8
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-06-10 16:56:39 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './sass/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
