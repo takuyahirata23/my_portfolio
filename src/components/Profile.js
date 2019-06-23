7c421e4c1c5455c02dc6c4a2af06d6b713eea616
Takuya Hirata <takuya.hirata@dcmail.ca>
2019-06-22 23:38:50 -0400

    start developing profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import takuya from './../img/takuya.jpg'
+import takuya from './../img/takuya_croped.jpg'
+import { IoIosCode, IoIosBook, IoMdPeople, IoMdLaptop} from 'react-icons/io'
 
 const Profile = () => {
   return (
     <div className="section-wrapper">
-      <div>
-        <img src={takuya} alt="Takuya"/>
+      <div className="profile-top">
+        <div className="profile-info">
+          <div className="profile-picture">
+            <img src={takuya} alt="Takuya"/>
+          </div>
+          <h1>TAKUYA HIRATA</h1>
+          <p>Web Developer</p>
+        </div>
+        <div className="profile-msg">
+          <p>While I was a student in college, my professor told me that "no websites are perfect, so always work to improve your projects and skills to do the best you can". I have decided to adopt this mentality and pursue this goal with passion.</p>
+          <p>
+            I like: playing baseball, watching hockey, listen to music, and learning new things.
+          </p>
+        </div>
+      </div>
+      <div className="profile-todo">
+        <h2>As A Developer</h2>
       </div>
     </div>
   )
