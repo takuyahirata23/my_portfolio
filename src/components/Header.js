ff00053dd8bac5e3be8db998572fa0237dd92320
Takuya Hirata <takuya.hirata@dcmail.ca>
2020-02-15 18:47:46 -0500

    user hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,21 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
-class Header extends Component {
-  state = {
-    isClicked: false
-  };
+const Header = () => {
+  const [isClicked, setIsClicked] = useState(false)
 
-  handleNav = () => {
-    this.setState(prev => ({
-      isClicked: !prev.isClicked
-    }));
-  };
-
-  resetNav = () => {
-    this.setState({
-      isClicked: false
-    });
-  };
-
-  render() {
-    return (
-      <header>
-        <div className="logo">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 126 130">
-            <path
-              fill="none"
-              stroke="white"
-              strokeWidth="3.5"
-              id="st1"
-              className="st1"
-              d="M97.51,16.12c-0.58-0.42-1.34-0.48-1.97-0.15c-0.63,0.32-1.03,0.97-1.03,1.69c0,0,0,13.55,0,18.36
+  return (
+    <header>
+      <div className="logo">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 126 130">
+          <path
+            fill="none"
+            stroke="white"
+            strokeWidth="3.5"
+            id="st1"
+            className="st1"
+            d="M97.51,16.12c-0.58-0.42-1.34-0.48-1.97-0.15c-0.63,0.32-1.03,0.97-1.03,1.69c0,0,0,13.55,0,18.36
             c0,1.87-0.11,3.55-0.38,4.97c-0.2,1.07-0.48,2-0.85,2.76c-0.56,1.15-1.25,1.94-2.26,2.52c-1.01,0.58-2.43,0.95-4.46,0.96
             c-4.95,0-8.89,0-11.6,0c-2.71,0-4.17,0-4.18,0c-0.5,0-0.99,0.2-1.34,0.55s-0.55,0.84-0.55,1.34v49.15
             c-1.81,0.75-3.09,2.53-3.09,4.61c0,2.75,2.23,4.98,4.98,4.98c2.75,0,4.98-2.23,4.98-4.98c0-2.08-1.28-3.86-3.09-4.61l0-47.25
@@ -39,15 +24,15 @@ class Header extends Component {
             c12.43,10.35,20.35,25.93,20.35,43.37c0,14.07-5.29,26.93-13.96,36.82c-7.71,8.79-18.09,15.21-29.77,18.01
             c-1.05-1.57-2.93-2.47-4.92-2.16c-2.72,0.43-4.58,2.98-4.15,5.7c0.43,2.72,2.98,4.58,5.7,4.15c2.13-0.33,3.72-1.97,4.11-3.97
             c12.52-2.97,23.63-9.83,31.87-19.23c9.24-10.54,14.9-24.29,14.9-39.32C122.43,44.84,112.6,27.05,97.51,16.12z"
-            />
+          />
 
-            <path
-              fill="none"
-              stroke="white"
-              strokeWidth="3.5"
-              className="st2"
-              id="st2"
-              d="M73.19,25.42c-2.08,0-3.86,1.28-4.61,3.09c-8.99,0-28.42,0-33.1,0c-1.65,0-3.17,0.36-4.49,1.01
+          <path
+            fill="none"
+            stroke="white"
+            strokeWidth="3.5"
+            className="st2"
+            id="st2"
+            d="M73.19,25.42c-2.08,0-3.86,1.28-4.61,3.09c-8.99,0-28.42,0-33.1,0c-1.65,0-3.17,0.36-4.49,1.01
             c-1.98,0.97-3.48,2.56-4.46,4.37c-0.98,1.82-1.47,3.89-1.47,5.95c0,2.72,0.85,5.49,2.74,7.64c0.94,1.07,2.15,1.97,3.58,2.59
             c1.43,0.62,3.08,0.95,4.9,0.95c5.04,0,11.36,0,14.46,0v69.12c-12.46-2.58-23.41-9.25-31.39-18.57C10.91,91.7,5.82,78.91,5.82,64.9
             c0-15.58,6.31-29.67,16.52-39.89C32.55,14.8,46.64,8.49,62.23,8.49c1.65,0,3.81,0.16,5.93,0.45c0.38,1.8,1.75,3.33,3.66,3.81
@@ -57,40 +42,38 @@ class Header extends Component {
             c-1.37,0-2.48-0.25-3.4-0.64c-1.37-0.6-2.34-1.53-3.02-2.7c-0.67-1.17-1.02-2.6-1.02-4.04c0-1.95,0.64-3.91,1.77-5.29
             c0.56-0.69,1.24-1.25,2.04-1.64c0.8-0.39,1.72-0.62,2.83-0.62c4.69,0,24.11,0,33.1,0c0.75,1.81,2.53,3.09,4.61,3.09
             c2.75,0,4.98-2.23,4.98-4.98S75.95,25.42,73.19,25.42z"
-            />
-          </svg>
+          />
+        </svg>
+      </div>
+      <nav>
+        <div
+          onClick={() => setIsClicked(!isClicked)}
+          className="hamburger-menu"
+        >
+          {isClicked ? (
+            <FaTimes className="menu-icon" />
+          ) : (
+            <FaBars className="menu-icon" />
+          )}
         </div>
-        <nav>
-          <div onClick={this.handleNav} className="hamburger-menu">
-            {this.state.isClicked ? (
-              <FaTimes className="menu-icon" />
-            ) : (
-              <FaBars className="menu-icon" />
-            )}
-          </div>
-          <ul
-            className={
-              this.state.isClicked
-                ? 'nav-animation-plus'
-                : 'nav-animation-minus'
-            }
-          >
-            <li onClick={this.resetNav}>
-              <NavLink exact to="/">
-                HOME
-              </NavLink>
-            </li>
-            <li onClick={this.resetNav}>
-              <NavLink to="/work">WORK</NavLink>
-            </li>
-            <li onClick={this.resetNav}>
-              <NavLink to="/profile">PROFILE</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+        <ul
+          className={isClicked ? 'nav-animation-plus' : 'nav-animation-minus'}
+        >
+          <li onClick={() => setIsClicked(false)}>
+            <NavLink exact to="/">
+              HOME
+            </NavLink>
+          </li>
+          <li onClick={() => setIsClicked(false)}>
+            <NavLink to="/work">WORK</NavLink>
+          </li>
+          <li onClick={() => setIsClicked(false)}>
+            <NavLink to="/profile">PROFILE</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,33 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import testimonials from './../testimonials.json'
 import { GoQuote } from 'react-icons/go'
 
-class Testimonials extends Component {
-
-  state = {
-    testimonials: []
-  }
-
-  componentDidMount() {
-    this.setState({
-      testimonials: testimonials
-    })
-  }
-
-  render() {
-    return (
-      <div className="testimonials-wrapper">
-        {this.state.testimonials.map(item => (
-          <div className="testimonial" key={item.name}>
-            <GoQuote className="quote-icon"/>
-            <p className="testimonial-msg">{item.testimonial}</p>
-            <h2>{item.name}</h2>
-            <p className="testimonial-company">{item.company}</p>
-          </div>
-        ))}
-      </div>
-    )
-  }
+const Testimonials = () => {
+  return (
+    <div className="testimonials-wrapper">
+      {testimonials.map(item => (
+        <div className="testimonial" key={item.name}>
+          <GoQuote className="quote-icon" />
+          <p className="testimonial-msg">{item.testimonial}</p>
+          <h2>{item.name}</h2>
+          <p className="testimonial-company">{item.company}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default Testimonials;
+export default Testimonials
